62cd41c6cb25805af6826e36d1c0947403c45189
AleckDHolly <aleckdavidholly@gmail.com>
2025-04-03 18:27:41 -0400

    NavigationLinks are no longer blue...

--- UI1/ContentView.swift
+++ UI1/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
                             Card(image: images[index])
                                 .matchedTransitionSource(id: "zoom\(index)", in: namespace)
                         }
+                        .buttonStyle(.plain)
                     }
                 }
             }
