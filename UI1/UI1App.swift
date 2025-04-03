2c11658bcdd1510fb2d7d2e220d2406d535d8340
AleckDHolly <aleckdavidholly@gmail.com>
2025-04-02 23:21:25 -0400

    Initial Commit

---
+++ UI1/UI1App.swift
@@ -0,0 +1,17 @@
+//
+//  UI1App.swift
+//  UI1
+//
+//  Created by Aleck David Holly on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct UI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
